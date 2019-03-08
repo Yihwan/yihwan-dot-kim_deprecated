fc0a2e97a6214d94d2a410f0d5be2c118337efb5
yihwan <hi@yihwan.kim>
2019-03-08 07:30:24 -0800

    fix column width

--- src/components/changelog/timeline/style.js
+++ src/components/changelog/timeline/style.js
@@ -19,14 +19,17 @@ export const TimelineItemContainer = styled.div`
 export const ItemMetaData = styled.div`
   margin-top: 1.5rem;
   display: flex;
+  flex: 1;
   color: ${COLORS.lightGrey};
 `;
 
 export const ItemIcon = styled.div`
+  flex: 0 0 1.5rem;
 `;
 
 export const ItemDate = styled.div`
-  margin: 0 0.75rem;
+  flex: 0 0 5rem;
+  text-align: center;
 
   ${MEDIA_QUERIES.smUp} {
     margin: 0 2rem;
@@ -34,6 +37,7 @@ export const ItemDate = styled.div`
 `;
 
 export const ItemText = styled.div`
+flex: 3;
   ${MEDIA_QUERIES.mdUp} {
     margin-left: 4rem;
   }
